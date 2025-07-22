7242799b14aa64e849a1a839405a7b1664e4d09f
Zene <oinkymail3@gmail.com>
2025-07-22 19:16:39 +0100

    Random monkeys on factory page

--- scripts/factory.js
+++ scripts/factory.js
@@ -1,3 +1,19 @@
+/**
+ * Get a random integer inside range [min, max].
+ * Courtesy of https://stackoverflow.com/a/1527820/12980669.
+ */
+function randomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+/**
+ * Equivalent of randomInt() but not constrained to integers
+ */
+function randomReal(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
 function sleep(ms)
 {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -13,7 +29,62 @@ setTimeout(async function() {
         lt.data = "/static/svg/linguini-toy.svg";
         lt.type = "image/svg+xml";
         
-        object.appendChild(lt);
+        var container = document.createElement("div");
+        container.classList.add("product-container");
+        container.appendChild(lt);
+        
+        object.appendChild(container);
         await sleep(500);
+    }
+}, 500);
+
+// random monkeys
+setTimeout(async function() {
+    var object = document.getElementById("factory");
+    
+    while (true)
+    {
+        var rand = randomInt(0, object.children.length - 1);
+        var product = object.children.item(rand);
+        
+        let start = product.getBoundingClientRect();
+        
+        if (product.children.length <= 1)
+        {
+            var monkey = document.createElement("img");
+            monkey.src = "/static/media/MONKEY.png";
+            monkey.classList.add("mini-monkey");
+            var x = -start.left;
+            let isLeft = randomInt(0, 1);
+            if (isLeft === 0)
+            {
+                x += window.innerWidth;
+            }
+            var y = -start.top;
+            let isTop = randomInt(0, 1);
+            if (isTop === 0)
+            {
+                y += window.innerHeight - 20;
+            }
+            
+            product.appendChild(monkey);
+            monkey.style.transform = "translate(" + x + "px," + y + "px) rotate(0deg)";
+            console.log(monkey.style.transform);
+            
+            let layout = document.body.offsetWidth;
+            
+            let end = randomInt(0, 1);
+            if (end === 0)
+            {
+                monkey.style.transform = "translate(-70px,-10px) rotate(720deg)";
+            }
+            else
+            {
+                monkey.style.transform = "translate(-40px,102px) rotate(720deg)";
+            }
+        }
+        
+        var time = randomInt(3000, 10000);
+        await sleep(time);
     }
 }, 500);
